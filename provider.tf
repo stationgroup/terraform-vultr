4564d1028a12918935e235704f1e9a56942104cb
Bryan Black <contact@reelsense.co>
2018-11-24 14:00:45 -0800

    add vultr provider template

---
+++ provider.tf
@@ -0,0 +1,6 @@
+# Configure the Vultr provider. 
+# Alternatively, export the API key as an environment variable: `export VULTR_API_KEY=<your-vultr-api-key>`.
+provider "vultr" {
+  api_key = "<your-vultr-api-key>"
+}
+
